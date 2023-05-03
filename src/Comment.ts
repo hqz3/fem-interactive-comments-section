7bea90a426b582217ed57c51d8a96a8ca2e8965e
hqz3 <heqzheng@gmail.com>
2023-05-02 21:57:57 -0400

    Create Comment class

---
+++ src/Comment.ts
@@ -0,0 +1,37 @@
+import { CommentType } from "./App";
+
+export class Comment {
+  element: HTMLElement;
+  comment: CommentType;
+
+  constructor(comment: CommentType) {
+    this.comment = comment;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("comment");
+
+    this.element.innerHTML = `
+          <div class="comment__user">
+            <img
+              class="comment__user-image"
+              src=${comment.user.image.png}
+              alt=${comment.user.username}
+            />
+            <span class="comment__username">${comment.user.username}</span>
+            <span class="comment__createdAt">${comment.createdAt}</span>
+          </div>
+          <p class="comment__text">
+            ${comment.content}
+          </p>
+          <div class="vote">
+            <button class="vote__upvote" aria-label='Upvote'>+</button>
+            <span class="vote__score" aria-label='Score'>${comment.score}</span>
+            <button class="vote__downvote" aria-label='Downvote'>-</button>
+          </div>
+          <div class="reply">
+            <img src="images/icon-reply.svg" alt="Reply" />
+            <button class="reply__button" aria-label='Reply'>Reply</button>
+          </div>
+    `;
+  }
+}
