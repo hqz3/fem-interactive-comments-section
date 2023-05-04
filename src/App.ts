1175eadde64414eea87c297cd263ab169ac7308c
hqz3 <heqzheng@gmail.com>
2023-05-04 00:26:10 -0400

    Implement helper function

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import serverData from "../data.json";
 import { Comment } from "./Comment";
+import { generateRespondElement } from "../utils/generateRespondElement";
 
 export type Data = {
   currentUser: User;
@@ -45,33 +46,14 @@ export class App {
     this.commentsEl.classList.add("comments");
     this.app.appendChild(this.commentsEl);
 
-    this.respondEl = document.createElement("form") as HTMLFormElement;
-    this.respondEl.classList.add("comment", "respond");
-
-    this.respondEl.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-
-    this.respondEl.innerHTML = `
-    <textarea
-      class="respond__textarea"
-      name="text"
-      placeholder="Add a comment..."
-    ></textarea>
-    <img
-      class="comment__user-image respond__user-image"
-      src="${this.currentUser.image.png}"
-      alt="${this.currentUser.username}"
-    />
-    <button class="respond__button" type="submit" aria-label="Respond">Send</button>
-  `;
+    this.respondEl = generateRespondElement(this.currentUser);
     this.commentsEl.appendChild(this.respondEl);
   }
 
   load() {
     this.comments.forEach((comment) => {
       const node = new Comment(this.currentUser, comment);
-      this.commentsEl.insertBefore(node.element, this.respondEl);
+      this.commentsEl.insertBefore(node.commentEl, this.respondEl);
     });
   }
 }
