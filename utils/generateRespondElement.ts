49fbe6eff3a2910f014eeec3d9e5d9bb25715b02
hqz3 <heqzheng@gmail.com>
2023-05-07 19:15:37 -0400

    Replies reference the comment's original author's username

--- utils/generateRespondElement.ts
+++ utils/generateRespondElement.ts
@@ -1,21 +1,28 @@
-import { User } from "../src/App";
+import { CommentType, User } from "../src/App";
 
-export const generateRespondElement = (currentUser: User) => {
+export const generateRespondElement = (
+  currentUser: User,
+  type: "Send" | "Reply" | "Update",
+  comment?: CommentType | null
+) => {
   const element = document.createElement("form") as HTMLFormElement;
   element.classList.add("comment__container", "respond");
+
+  const isCurrentUser = currentUser.username == comment?.user.username;
+  const atUsername = `@${comment?.user.username}`;
 
   element.innerHTML = `
   <textarea
     class="respond__textarea"
     name="text"
     placeholder="Add a comment..."
-  ></textarea>
+  >${type === "Reply" ? atUsername : ""} </textarea>
   <img
     class="comment__user-image respond__user-image"
     src="${currentUser.image.png}"
     alt="${currentUser.username}"
   />
-  <button class="respond__button" type="submit" aria-label="Respond">Send</button>
+  <button class="respond__button" type="submit" aria-label=${type}>${type}</button>
 `;
 
   return element;
