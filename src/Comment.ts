e960368d2593c503dde6fc2276b2dfae45db085a
hqz3 <heqzheng@gmail.com>
2023-05-07 01:06:21 -0400

    Implement new reply feature

--- src/Comment.ts
+++ src/Comment.ts
@@ -2,6 +2,7 @@ import { User, CommentType } from "./App";
 import { Reply } from "./Reply";
 import { createElement } from "../utils/createElement";
 import { generateCommentElements } from "../utils/generateCommentElements";
+import { generateRespondElement } from "../utils/generateRespondElement";
 
 export class Comment {
   currentUser: User;
@@ -10,20 +11,26 @@ export class Comment {
 
   commentEl: HTMLElement;
   commentContainerEl: HTMLElement;
+  replyPanel: HTMLElement | null;
   repliesContainerEl: HTMLElement;
 
   constructor(currentUser: User, comment: CommentType) {
     this.currentUser = currentUser;
     this.comment = comment;
     this.vote = 0;
+    this.replyPanel = null;
 
     this.commentEl = createElement("comment");
     this.commentContainerEl = createElement("comment__container");
     this.repliesContainerEl = createElement("replies__container");
 
-    this.renderUserComment();
+    this.commentEl.appendChild(this.commentContainerEl);
+    this.commentEl.appendChild(this.repliesContainerEl);
 
-    this.commentContainerEl.addEventListener("click", (e: Event) => {
+    this.renderUserComment();
+    this.loadReplies();
+
+    this.commentEl.addEventListener("click", (e: Event) => {
       const target = e.target as HTMLElement;
       if (target.classList.contains("vote__upvote") && this.vote <= 0) {
         this.comment.score += 1;
@@ -39,10 +46,24 @@ export class Comment {
       }
     });
 
-    this.commentEl.appendChild(this.commentContainerEl);
-    this.commentEl.appendChild(this.repliesContainerEl);
+    const replyButton = this.commentContainerEl.querySelector(
+      ".comment__reply"
+    ) as HTMLElement;
 
-    this.loadReplies();
+    if (replyButton) {
+      replyButton.addEventListener("click", () => {
+        if (this.replyPanel) return this.removeReplyPanel();
+
+        const replyPanel = this.showReplyPanel();
+        replyPanel.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const target = e.target as HTMLFormElement;
+          const text = target.text.value;
+          this.addReply(text);
+          this.removeReplyPanel();
+        });
+      });
+    }
   }
 
   loadReplies() {
@@ -57,5 +78,42 @@ export class Comment {
       this.currentUser,
       this.comment
     );
+  }
+
+  showReplyPanel() {
+    this.replyPanel = generateRespondElement(this.currentUser, "Reply");
+    this.replyPanel.classList.add("slideDown");
+    this.repliesContainerEl.insertAdjacentElement(
+      "beforebegin",
+      this.replyPanel
+    );
+    return this.replyPanel;
+  }
+
+  removeReplyPanel() {
+    if (this.replyPanel) {
+      this.replyPanel.remove();
+      this.replyPanel = null;
+    }
+  }
+
+  addReply(reply: string) {
+    if (!reply.length) return;
+
+    const newReply = {
+      id: Date.now(),
+      user: this.currentUser,
+      createdAt: "Just now",
+      content: reply,
+      score: 0,
+      replies: [],
+    };
+
+    const instance = new Reply(this.currentUser, newReply);
+    this.repliesContainerEl.appendChild(instance.replyEl);
+    instance.replyEl.classList.add("slideDown");
+    setTimeout(() => {
+      instance.replyEl.classList.remove("slideDown");
+    }, 500);
   }
 }
--- src/Reply.ts
+++ src/Reply.ts
@@ -1,11 +1,13 @@
 import { User, CommentType } from "./App";
 import { createElement } from "../utils/createElement";
 import { generateCommentElements } from "../utils/generateCommentElements";
+import { generateRespondElement } from "../utils/generateRespondElement";
 
 export class Reply {
   currentUser: User;
   reply: CommentType;
   replyEl: HTMLElement;
+  replyPanel: HTMLElement | null;
   replyContainerEl: HTMLElement;
   vote: -1 | 0 | 1;
 
@@ -13,9 +15,12 @@ export class Reply {
     this.currentUser = currentUser;
     this.reply = reply;
     this.vote = 0;
+    this.replyPanel = null;
 
     this.replyEl = createElement("comment", "reply");
     this.replyContainerEl = createElement("comment__container");
+    this.replyEl.appendChild(this.replyContainerEl);
+
     this.renderReply();
 
     this.replyContainerEl.addEventListener("click", (e: Event) => {
@@ -34,7 +39,24 @@ export class Reply {
       }
     });
 
-    this.replyEl.appendChild(this.replyContainerEl);
+    const replyButton = this.replyContainerEl.querySelector(
+      ".comment__reply"
+    ) as HTMLElement;
+
+    if (replyButton) {
+      replyButton.addEventListener("click", (e) => {
+        if (this.replyPanel) return this.removeReplyPanel();
+
+        const replyPanel = this.showReplyPanel();
+        replyPanel.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const target = e.target as HTMLFormElement;
+          const text = target.text.value;
+          this.addReply(text);
+          this.removeReplyPanel();
+        });
+      });
+    }
   }
 
   renderReply() {
@@ -42,5 +64,44 @@ export class Reply {
       this.currentUser,
       this.reply
     );
+  }
+
+  showReplyPanel() {
+    this.replyPanel = generateRespondElement(this.currentUser, "Reply");
+    this.replyPanel.classList.add("slideDown");
+    this.replyContainerEl.insertAdjacentElement("afterend", this.replyPanel);
+    return this.replyPanel;
+  }
+
+  removeReplyPanel() {
+    if (this.replyPanel) {
+      this.replyPanel.remove();
+      this.replyPanel = null;
+    }
+  }
+
+  addReply(reply: string) {
+    if (!reply.length) return;
+
+    const newReply = {
+      id: Date.now(),
+      user: this.currentUser,
+      createdAt: "Just now",
+      content: reply,
+      score: 0,
+      replies: [],
+    };
+
+    const instance = new Reply(this.currentUser, newReply);
+    if (this.replyEl && this.replyEl.parentNode) {
+      (this.replyEl.parentNode as HTMLElement).insertAdjacentElement(
+        "beforeend",
+        instance.replyEl
+      );
+      instance.replyEl.classList.add("slideDown");
+      setTimeout(() => {
+        instance.replyEl.classList.remove("slideDown");
+      }, 500);
+    }
   }
 }
